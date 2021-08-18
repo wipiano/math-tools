8cb34864f22f6244d74fe7058bf444499bdae39e
Kohei Hakoishi <wipiano@gmail.com>
2021-08-18 22:55:11 +0900

    console app ひながた

--- src/dotnet/MathTools/Program.cs
+++ src/dotnet/MathTools/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+await Host.CreateDefaultBuilder()
+    .RunConsoleAppFrameworkAsync(args);
